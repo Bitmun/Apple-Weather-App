f8d7cddb0ef1bd0c93999771d04daad664b0aa5d
Bitmun <pashabalko21@gmail.com>
2024-02-06 11:43:37 +0300

    feat: add bg video for city card

--- src/components/CityCard/CityCard.tsx
+++ src/components/CityCard/CityCard.tsx
@@ -3,6 +3,7 @@ import styles from "./cityCard.module.css";
 import { WeatherDataContext } from "../../App";
 import { TempBlock } from "../TempBlock/TempBlock";
 import { formatTime } from "../../utils/dataUtils";
+import videoBg from "../../assets/mp4/bgRain_optimized.mp4";
 
 export function CityCard() {
   const data = useContext(WeatherDataContext);
@@ -27,17 +28,20 @@ export function CityCard() {
   );
 
   return (
-    <div className={styles.cardWrapper}>
-      <div className={styles.leftPartWrapper}>
-        <p className={styles.cityName}>New York</p>
-        <p className={styles.cityTime}>{currentTime}</p>
-        <p className={styles.weatherCondition}>Partly Cloudy</p>
+    <div className={styles.cardContainer}>
+      <video className={styles.bgVideo} src={videoBg} autoPlay loop muted />
+      <div className={styles.cardWrapper}>
+        <div className={styles.leftPartWrapper}>
+          <p className={styles.cityName}>New York</p>
+          <p className={styles.cityTime}>{currentTime}</p>
+          <p className={styles.weatherCondition}>Partly Cloudy</p>
+        </div>
+        <TempBlock
+          maxTemp={maxTemp}
+          minTemp={minTemp}
+          currentTemp={currentTemp}
+        />
       </div>
-      <TempBlock
-        maxTemp={maxTemp}
-        minTemp={minTemp}
-        currentTemp={currentTemp}
-      />
     </div>
   );
 }
